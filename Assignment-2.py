e908583ae3127f66a7347ad7acaea00f9693ae39
Rahul Kinra <38756353+rahulkinra@users.noreply.github.com>
2018-08-18 01:41:09 +0530

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,31 @@
+"""Ques 1"""
+qwert='Hey Stalker'
+print(qwert)
+
+"""Ques 2"""
+s1='You are '
+s2='Awesome.'
+s3=s1+s2
+print(s3)
+
+"""Ques 3"""
+x=int(input("Enter first value"))
+y=int(input("Enter second value"))
+z=int(input("Enter third value"))
+print("First value is %d, Second value is %d and Third value is %d" %(x,y,z))
+
+"""Ques 4"""
+awsm='Let\'s get started'
+print(awsm)
+
+"""Ques 5"""
+a='Acadview'
+b='Python'
+c=5000
+print("Name: %s , Course: %s , Fees: %d" %(a,b,c))
+
+"""Ques 6"""
+radius=float(input("Enter radius of circle"))
+pi=3.14
+area=pi*(radius**2)
+print("Area of circle with radius " , radius , " is " , area)
